ba567b13f891c7d12fef922d3b591dddf593fe01
shiroyasha68 <wira.yudha68@gmail.com>
2022-08-09 19:28:59 +0800

    add required to title

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -6,6 +6,7 @@ const AddNotes = ({addNotes}) => {
     const [addBody, setAddbody] = useState('');
     const [charLimit, setCharLimit] = useState(0);
     const [colorPicker, setColorPicker] = useState('f77f00');
+    const [requiredTitle, setRequiredTitle] = useState(false)
     const addTitleFun = (e) => {
         setAddTitle(event.target.value.slice(0, 50));
         setCharLimit(event.target.value.length);
@@ -28,33 +29,42 @@ const AddNotes = ({addNotes}) => {
         backgroundColor:`#${colorPicker}`
     }
     const addNotesFun = () => {
-        const newObj = {
-            id: +new Date(),
-            title: addTitle,
-            body: addBody,
-            createdAt: Date(),
-            color: colorPicker,
-            archived: false,
+        if(addTitle.length > 0) {
+            const newObj = {
+                id: +new Date(),
+                title: addTitle,
+                body: addBody,
+                createdAt: Date(),
+                color: colorPicker,
+                archived: false,
+            }
+           addNotes(newObj);
+           setAddTitle('');
+           setAddbody('');
+           setCharLimit(0);
+           setRequiredTitle(false)
         }
-       addNotes(newObj);
-       setAddTitle('');
-       setAddbody('');
-       setCharLimit(0);
+        else {
+            setRequiredTitle(true)
+        }
     }
     return (
         <div className="add-notes" style={myStyle}>
-            <div className="rounded">{charValue()}</div>
-            <input type="text" name="" className="input-title" placeholder="Judul" onChange={addTitleFun} value={addTitle} style={myStyle}/><br />
-            <textarea name="" id="" cols="30" rows="4" placeholder="Catatan" onChange={addbodyFun} value={addBody} style={myStyle} />
-            <div className="button-area">
-                <div className="btn-logo">
-                    <NoteColor addColorFun={addColorFun} />
+                <input type="text" name="" className="input-title" placeholder="Judul" onChange={addTitleFun} value={addTitle} style={myStyle}/><br />
+                {requiredTitle ? 
+                <p>Judul harus diisi</p>
+                 : null}
+                
+                <textarea name="" id="" cols="30" rows="4" placeholder="Catatan" onChange={addbodyFun} value={addBody} style={myStyle} />
+                <div className="button-area">
+                    <div className="btn-logo">
+                        <NoteColor addColorFun={addColorFun} />
+                    </div>
+                    <div className="btn-submit">
+                        <button className="submit-note" onClick={addNotesFun} style={myStyle}>Simpan</button>
+                        <button className="cancel-note" style={myStyle}>Batalkan</button>
+                    </div>
                 </div>
-                <div className="btn-submit">
-                    <button className="submit-note" onClick={addNotesFun} style={myStyle}>Simpan</button>
-                    <button className="cancel-note" style={myStyle}>Batalkan</button>
-                </div>
-            </div>
         </div>
     )
 }
